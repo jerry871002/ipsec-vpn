138a513401f5e9dac408d508f20d49db1a84165e
Jerry Yang <jerry871002@gmail.com>
2023-05-16 22:04:55 +0800

    move routing decision to gateway-s
    security enhancement: if client gateway is compromised, it won't
    be able to access other client's server

--- scripts/site_a_gateway.sh
+++ scripts/site_a_gateway.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+##### Install swanctl #####
+
+apt-get update -y
+apt-get install -y strongswan-swanctl charon-systemd
+
 ##### Setup iptables rules #####
 
 # NAT traffic going to the internet
@@ -9,17 +14,13 @@ iptables -t nat -A POSTROUTING -o enp0s8 -j MASQUERADE
 # Redirect traffic that was going to the local server to the cloud server
 # DNAT stands for Destination NAT
 ip addr add 10.1.0.99/16 dev enp0s9
-iptables -t nat -A PREROUTING -p tcp -d 10.1.0.99 --dport 8080 -j DNAT --to-destination 172.48.48.51:8080
+iptables -t nat -A PREROUTING -p tcp -d 10.1.0.99 --dport 8080 -j DNAT --to-destination 172.30.30.30:8080
 
 # Save the iptables rules
 iptables-save > /etc/iptables/rules.v4
 ip6tables-save > /etc/iptables/rules.v6
 
 ##### Setup strongSwan site-to-site VPN #####
-
-# install swanctl
-apt-get update -y
-apt-get install -y strongswan-swanctl charon-systemd
 
 # Root CA certificate
 cp /vagrant/pki/cert/rootCACert.pem /etc/swanctl/x509ca/
