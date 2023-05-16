7515996a92fd213dc1db34bb1a853c7c2349a7b2
Jerry Yang <jerry871002@gmail.com>
2023-05-16 22:07:13 +0800

    containerize servers

--- scripts/cloud_server.sh
+++ scripts/cloud_server.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
+# Install docker
+apt-get update -y
+apt-get install -y docker.io
+
+## Build app image
+cp /vagrant/scripts/Dockerfile /home/vagrant/server_app
+cd /home/vagrant/server_app
+docker build -t server-app:v1 .
+
 ## Traffic going to the internet
-route add default gw 172.48.48.49
+route add default gw 10.1.0.1
 
 ## Save the iptables rules
 iptables-save > /etc/iptables/rules.v4
 ip6tables-save > /etc/iptables/rules.v6
 
-## Install app
-cd /home/vagrant/server_app
-npm install
+# Run app
+docker run -p 8080:8080 -d --name site-a-server server-app:v1
+docker run -p 8081:8080 -d --name site-b-server server-app:v1
